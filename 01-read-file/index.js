ae24dbcf4fd6219b9824f21d0056508d00865139
Maria <sheyko.mari@mail.ru>
2022-05-20 23:33:44 +0300

    feat: first task done

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+
+const file = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(file, 'utf-8');
+
+readableStream.on('data', (data) => console.log(data));
