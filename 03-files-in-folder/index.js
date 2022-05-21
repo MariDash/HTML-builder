87e766bb79e6607c25c34499c6a5b7177ede580b
Maria <sheyko.mari@mail.ru>
2022-05-21 22:24:59 +0300

    feat: third task done

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(
+  path.join(__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      const filePermission = path.extname(file.name);
+      const fileName = path.basename(file.name, filePermission);
+      fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, file) => {
+        if (err) throw err;
+
+        const fileSize = file.size / 1000;
+        if (file.isFile()) {
+          console.log(`${fileName} - ${filePermission} - ${fileSize}kb`);
+        }
+      });
+    });
+  }
+);
